6c4d7eb3b3e1dff3317c80302bae3c8813486165
Robbie S <robbie@selwynsoftware.com>
2015-12-26 18:36:48 -0800

    added main files

---
+++ src/Generator.java
@@ -0,0 +1,21 @@
+
+public class Generator {
+	
+	//Constructor if no restrictions on color
+	public Generator(){
+		
+	}
+	
+	
+	/*
+	 * Constructor if restrictions on the colors
+	 * @param	rlow: The low value of the range for the red value
+	 * @param	rhigh: The high value of the range for the red value
+	 * @param	glow: The low value of the range for the green value
+	 * @param	ghigh: The high value of the range for the green value
+	 */
+	public Generator(int rlow, int rhigh, int glow, int ghigh, int blow, int bhigh){
+		
+	}
+	
+}
